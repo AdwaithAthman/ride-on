1f488761c24d84069b90cffe19bd8447f70ce385
AdwaithAthman <iamadwaith17@gmail.com>
2024-10-28 02:11:14 +0530

    added image and text on onboarding page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,57 @@
+import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
+import { Text, TouchableOpacity, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Haptics from "expo-haptics";
+import Swiper from "react-native-swiper";
+import { useRef, useState } from "react";
+import { onboarding } from "@/constants";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const handleIndexChange = (index: number) => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
+    setActiveIndex(index);
+  };
   return (
-    <SafeAreaView>
-      <Text>Onboarding</Text>
+    <SafeAreaView className="flex h-full items-center justify-center bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Rigid);
+          router.replace("/(auth)/sign-in");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="w-[32px] h-2 mx-1 bg-[#E2E8F0] rounded-full" />}
+        activeDot={
+          <View className="w-[32px] h-2 mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={handleIndexChange}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className=" text-lg font-JakartaSemiBold mx-10 mt-3 text-center text-[#858585">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
       <StatusBar style="dark" />
     </SafeAreaView>
   );
