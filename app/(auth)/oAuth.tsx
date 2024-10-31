09ba13c0c13111c543571601e1b04956d09ddc5b
Adwaith Athman <iamadwaith17@gmail.com>
2024-11-01 02:47:21 +0530

    Updated oAuth google button

--- app/(auth)/oAuth.tsx
+++ app/(auth)/oAuth.tsx
@@ -1,13 +1,28 @@
-import { View } from "react-native"
+import CustomButton from "@/components/customButton"
+import { View, Text, Image } from "react-native"
+import { icons } from "@/constants"
+import * as Haptics from "expo-haptics"
 
 const OAuth = () => {
+    const handleGoogleSignIn = async() => {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
+        console.log("Login with Google")
+    }
     return (
         <View>
             <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
                 <View className="flex-1 h-[1px] bg-neutral-100" />
-                <View className="text-lg">Or</View>
+                <Text className="text-lg">Or</Text>
                 <View className="flex-1 h-[1px] bg-neutral-100" />
             </View>
+
+            <CustomButton
+                title="Login with Google"
+                className="mt-5 w-full shadow-none"
+                IconLeft={() => <Image source={icons.google} resizeMode="contain" className="w-5 h-5 mx-2" />} 
+                bgVariant="outline"
+                textVariant="primary"
+                onPress={handleGoogleSignIn} />
         </View>
     )
 }
