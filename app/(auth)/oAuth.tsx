36a03f3a8d90a3330a51ccdb065c1c2aeeeb4e74
Adwaith Athman <iamadwaith17@gmail.com>
2024-10-31 23:50:35 +0530

    Created oAuth component

---
+++ app/(auth)/oAuth.tsx
@@ -0,0 +1,13 @@
+import { View } from "react-native"
+
+const oAuth = () => {
+    return (
+        <View>
+            <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
+                <View className="flex-1 h-[1px] bg-neutral-100" />
+                <View className="text-lg">Or</View>
+                <View className="flex-1 h-[1px] bg-neutral-100" />
+            </View>
+        </View>
+    )
+}
