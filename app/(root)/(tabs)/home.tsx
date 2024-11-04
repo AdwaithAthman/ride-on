7602ffcf79f7a7966bb7cf1b65acf26efaa355ed
Adwaith Athman <iamadwaith17@gmail.com>
2024-11-04 12:49:57 +0530

    Done authentication by email and redirecting to home

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,13 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-const Home = () => {
+export default function Home() {
+  const { user } = useUser()
+
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign In</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign Up</Text>
+        </Link>
+      </SignedOut>
     </SafeAreaView>
-  );
-};
-
-export default Home;
+  )
+}
