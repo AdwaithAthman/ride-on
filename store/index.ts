ad07d828c3890fe2f8170bc97bb32eceb7be3df7
Adwaith Athman <iamadwaith17@gmail.com>
2024-11-06 12:37:57 +0530

    Created locationStore using zustand

---
+++ store/index.ts
@@ -0,0 +1,23 @@
+import { LocationStore } from "@/types/type";
+import { create } from "zustand";
+
+export const useLocationStore = create<LocationStore>((set) => ({
+  userAddress: null,
+  userLongitude: null,
+  userLatitude: null,
+  destinationAddress: null,
+  destinationLongitude: null,
+  destinationLatitude: null,
+  setUserLocation: ({ latitude, longitude, address }) =>
+    set({
+      userLatitude: latitude,
+      userLongitude: longitude,
+      userAddress: address,
+    }),
+  setDestinationLocation: ({ latitude, longitude, address }) =>
+    set({
+      destinationLatitude: latitude,
+      destinationLongitude: longitude,
+      destinationAddress: address,
+    }),
+}));
