c59aef4862fd20e70eff160551e935f74a15ff7b
Adwaith Athman <iamadwaith17@gmail.com>
2024-11-07 12:53:41 +0530

    Done GoogleTextInput

--- components/googleTextInput.tsx
+++ components/googleTextInput.tsx
@@ -1,6 +1,11 @@
-import { View, Text } from "react-native";
+import "react-native-get-random-values";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { GoogleInputProps } from "@/types/type";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { icons } from "@/constants";
+
+const googlePlacesApiKey = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
 
 export default function GoogleTextInput({
   icon,
@@ -10,8 +15,64 @@ export default function GoogleTextInput({
   handlePress,
 }: GoogleInputProps) {
   return (
-    <View className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}>
-      <Text>Search</Text>
+    <View
+      className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
+    >
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Where you want to go?"
+        debounce={200}
+        onPress={(data, details = null) => {
+          handlePress({
+            latitude: details?.geometry.location?.lat!,
+            longitude: details?.geometry.location?.lng!,
+            address: data.description,
+          })
+        }}
+        query={{
+          key: googlePlacesApiKey,
+          language: "en",
+          components: "country:in"
+        }}
+        renderLeftButton={() => (
+          <View className="justify-center items-center w-6 h-6">
+            <Image source={icon ? icon : icons.search } className="w-6 h-6" resizeMode="contain" />
+          </View>
+        )}
+        textInputProps={{
+          placeholderTextColor: "gray",
+          placeholder: initialLocation ?? "Where do you want to go?",
+        }}
+        styles={{
+          container: {
+            position: "relative",
+            shadowColor: "#d4d4d4",
+            borderRadius: 20,
+          },
+          textInputContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            marginHorizontal: 20,
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor || "white",
+            fontSize: 16,
+            fontWeight: "600",
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor || "white",
+            position: "relative",
+            top: 0,
+            width: "100%",
+            borderRadius: 10,
+            shadowColor: "#d4d4d4",
+            zIndex: 99,
+          }
+        }}
+      />
     </View>
   );
 }
