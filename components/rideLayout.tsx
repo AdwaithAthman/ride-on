3b34f9fd64285f019efb92b741ec5f6d9758f867
Adwaith Athman <iamadwaith17@gmail.com>
2024-11-10 20:28:36 +0530

    Added haptics for bottomsheet snap points

--- components/rideLayout.tsx
+++ components/rideLayout.tsx
@@ -1,37 +1,56 @@
-import { icons } from "@/constants"
-import { router } from "expo-router"
-import { View, Text, TouchableOpacity, Image } from "react-native"
-import { GestureHandlerRootView } from "react-native-gesture-handler"
-import Map from "./map"
-import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet"
-import { useRef } from "react"
+import { icons } from "@/constants";
+import { router } from "expo-router";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Map from "./map";
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import { useRef } from "react";
+import * as Haptics from "expo-haptics";
 
-const RideLayout = ({ children, title }: { children: React.ReactNode, title?: string }) => {
-    const bottomSheetRef = useRef<BottomSheet>(null);
-    return (
-        <GestureHandlerRootView>
-            <View className="flex-1 bg-white">
-                <View className="flex flex-col h-full bg-blue-500">
-                    <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
-                        <TouchableOpacity onPress={() => router.back()}>
-                            <View className="w-10 h-10 rounded-full bg-white items-center justify-center">
-                                <Image source={icons.backArrow} resizeMode="contain" className="w-6 h-6" />
-                            </View>
-                        </TouchableOpacity>
-                        <Text className="text-xl font-JakartaSemiBold ml-5">
-                            {title || "Go Back"}
-                        </Text>
-                    </View>
-                    <Map />
-                </View>
-                <BottomSheet ref={bottomSheetRef} snapPoints={["40%", "85%"]} index={0}>
-                    <BottomSheetScrollView style={{ flex: 1, padding: 20 }} >
-                        {children}
-                    </BottomSheetScrollView>
-                </BottomSheet>
-            </View>
-        </GestureHandlerRootView>
-    )
-}
+const RideLayout = ({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const handleSheetChange = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Rigid);
+  };
+  return (
+    <GestureHandlerRootView>
+      <View className="flex-1 bg-white">
+        <View className="flex flex-col h-full bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="w-10 h-10 rounded-full bg-white items-center justify-center">
+                <Image
+                  source={icons.backArrow}
+                  resizeMode="contain"
+                  className="w-6 h-6"
+                />
+              </View>
+            </TouchableOpacity>
+            <Text className="text-xl font-JakartaSemiBold ml-5">
+              {title || "Go Back"}
+            </Text>
+          </View>
+          <Map />
+        </View>
+        <BottomSheet
+          ref={bottomSheetRef}
+          snapPoints={["40%", "85%"]}
+          index={0}
+          onChange={handleSheetChange}
+        >
+          <BottomSheetScrollView style={{ flex: 1, padding: 20 }}>
+            {children}
+          </BottomSheetScrollView>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
 
-export default RideLayout
+export default RideLayout;
